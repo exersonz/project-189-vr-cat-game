5f458ad069f5abcd7dc6a623aa5025aba2964e63
exersonz <86198455+exersonz@users.noreply.github.com>
2023-07-08 18:23:57 -0700

    Add files via upload

--- clickEvents.js
+++ clickEvents.js
@@ -1,11 +1,8 @@
-AFRAME.registerComponent("tool-clicked", {
+AFRAME.registerComponent("cursor-listener", {
     init: function(){
-        this.toolClicked();
-    },
-    toolClicked: function(){
-        var bathe = document.getElementById("bathe");
-        bathe.addEventListener("click", function(){
-            alert("THE BATHE TOOL IS CLICKED!!!!!!!")
-        });
+       var el = this.el;
+       el.addEventListener("click", function(){
+           alert("THE " + el.getAttribute("id").toUpperCase() + " IS CLICKED!");
+       });
     }
 });
